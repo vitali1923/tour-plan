ecaf3cde35f1b6ed42b9fee842787f3f75f3417c
vitali1923 <vitalimelnik@yandex.ru>
2021-11-24 14:26:56 +0300

    create: добавил блок reviews

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
     effect:'fade',
+  });
+  
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect:'flip',
   });
