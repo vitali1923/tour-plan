344c7319ddcad64ae8a6ce871a033fe11c2c5443
vitali1923 <vitalimelnik@yandex.ru>
2021-11-30 10:57:16 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -21,3 +21,9 @@ const hotelSlider = new Swiper('.hotel-slider', {
     },
     effect:'flip',
   });
+
+  var menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', function() {
+    console.log('Клик по кнопке меню');
+    document.querySelector('.navbar-button').classList.toggle('navbar-button--visible');
+  });
