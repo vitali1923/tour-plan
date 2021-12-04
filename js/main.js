156a418df7bd2611a436b013234ba5203a805133
vitali1923 <vitalimelnik@yandex.ru>
2021-12-04 16:40:13 +0300

    Create: добавил модельное окно

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function(){
+  const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
@@ -8,6 +9,7 @@ const hotelSlider = new Swiper('.hotel-slider', {
       prevEl: '.hotel-slider__button--prev',
     },
     effect:'fade',
+    keyboard: true,
   });
   
   const reviewsSlider = new Swiper('.reviews-slider', {
@@ -21,9 +23,32 @@ const hotelSlider = new Swiper('.hotel-slider', {
     },
     effect:'flip',
   });
+  
 
-  var menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', function() {
-    console.log('Клик по кнопке меню');
-    document.querySelector('.navbar-button').classList.toggle('navbar-button--visible');
+  var menuButton = $('.menu-button');
+  menuButton.on('click', function() {
+    $('.navbar-button').toggleClass('navbar-button--visible');
   });
+
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal(){
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+});
